7191c71c0b24f274be52b127242beb93f3b684a9
alce65 <alce65@hotmail.es>
2019-04-22 11:30:56 +0200

    Variables

---
+++ JS/02_variables.js
@@ -0,0 +1,46 @@
+// En ES6 se utizan let y const para declarar variables
+
+let nombre // Declarar
+nombre = 'Pepe' // Inicializar
+
+let edad = 34 // Declarar e inicializar
+
+// Tipos de datos
+
+// undefined
+let data
+console.log(data)
+console.log(typeof data)
+
+
+// strings
+data = 'Hola amigos'
+console.log(data)
+console.log(typeof data)
+
+// numbers
+data = 35
+console.log(data)
+console.log(typeof data)
+
+// booleans
+data = true
+data = false
+console.log(data)
+console.log(typeof data)
+
+// object
+data = { name: 'Pepe', age: 34}
+console.log(data)
+console.log(typeof data)
+
+// object tipo array
+data = [23, 45, 78]
+data = ['Lunes', 'Martes', 23, 78, true, undefined, {}]
+console.log(data)
+console.log(typeof data)
+
+function mostrar() {}
+
+console.log(mostrar)
+console.log(typeof mostrar)
