be87a7193d5b84efac1ef1bd0656cbd02045d6f8
alce65 <alce65@hotmail.es>
2019-05-08 10:12:52 +0200

    Letra del DNI

---
+++ JS/32_dni.js
@@ -0,0 +1,65 @@
+/**
+ * Programa que comprueba la letra de un DNI
+ * La letra se asigna a partir de un array preestablecido, 
+ * seleccionando la posicion correspondiente al desto del numero entre 23
+ * 
+ */
+
+ /**
+  * function calcularLetraDNI
+  * @param dni: string
+  * @returns : string
+  * 
+  */
+
+  function calcularLetraDNI(dni = '') {
+      letrasDNI = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 
+      'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+      // let num = dni % 23
+      // return letrasDNI[num]
+      return letrasDNI[dni % 23]
+  }
+
+  /**
+   * function isLetraDNIValida
+   * @param dni: string (formato nnnnnnnnL)
+   * @returns : boolean
+   * 
+   */
+
+   //    1R - length = 2
+   //   99999999R - length = 9
+
+  function isLetraDNIValida(dni = '') {
+      r = false
+      let letra = dni[dni.length-1]
+      let numero = dni.slice(0, dni.length-1)
+      numero = formateaNumero(numero)
+      if (letra === calcularLetraDNI(numero)) { r = true}
+      return r
+  }
+
+  /**
+   * function formateaNumero
+   * @param cadena: string
+   * @returns : string
+   *  
+   */
+    function formateaNumero(cadena = '') {
+        let aCadena = cadena.split('')
+        for (let i = 0; i < aCadena.length; i++) {
+            const item = aCadena[i];
+            if (isNaN(item)) {
+                aCadena.splice(i,1)
+            }
+        }
+        return aCadena.join('')
+    }
+
+
+  (function () {
+      let miDNI = '50.821.728  / P'
+      // console.log(cacalcularLetraDNI(miDNI))
+      console.log(isLetraDNIValida(miDNI))
+      //console.log(formateaNumero(miDNI))
+  })()
