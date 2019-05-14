19e23aa91fd9c2a175cfc95bd0b906c1faf62bca
alce65 <alce65@hotmail.es>
2019-05-14 13:59:04 +0200

    Añadir boton borrar

--- JS_Web/05_interactivo_ES6/js/saludador.js
+++ JS_Web/05_interactivo_ES6/js/saludador.js
@@ -7,22 +7,33 @@ export class Saludador {
         this.inNombre = document.querySelector('#in-nombre')
         this.btnSaludar = document.querySelector('#btn-saludar')
         this.btnDespedirse = document.querySelector('#btn-despedirse')
+        this.btnBorrar = document.querySelector('#btn-borrar')
         this.output = document.querySelector('#output')
         
         /**Asignar manejadores a los nodos */
         this.btnSaludar.addEventListener('click', this.saludar.bind(this))
         this.btnDespedirse.addEventListener('click', this.despedirse.bind(this))
+        this.btnBorrar.addEventListener('click', this.borrar.bind(this))
     }
 
     saludar() {
-        this.aQuien = '<b>' + this.inNombre.value + '</b>'
-        // console.log(this.msg[0] + this.aQuien)
-        this.output.innerHTML = this.msg[0] + this.aQuien
+        if (this.inNombre.value) {
+            this.aQuien = '<b>' + this.inNombre.value + '</b>'
+            // console.log(this.msg[0] + this.aQuien)
+            this.output.innerHTML = this.msg[0] + this.aQuien
+        }
     }
 
     despedirse() {
-        this.aQuien = '<b>' + this.inNombre.value + '</b>'
-        // console.log(this.msg[1] + this.aQuien)
-        this.output.innerHTML = this.msg[1] + this.aQuien
+        if (this.inNombre.value) {
+            this.aQuien = '<b>' + this.inNombre.value + '</b>'
+            // console.log(this.msg[1] + this.aQuien)
+            this.output.innerHTML = this.msg[1] + this.aQuien            
+        }
+    }
+
+    borrar() {
+        this.output.innerHTML = ''
+        this.inNombre.value = ''
     }
 }
