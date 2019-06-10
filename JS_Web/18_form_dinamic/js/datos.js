676cc261762c59aaa142370babcc33a8c5e79fd4
alce65 <alce65@hotmail.es>
2019-06-10 13:01:52 +0200

    Datos formulario

---
+++ JS_Web/18_form_dinamic/js/datos.js
@@ -0,0 +1,18 @@
+export const GENEROS = [
+    {value:'', label: ''},
+    {value:'P', label: 'Policiaco', autores : [
+        {value:'Christie,Agata', label: 'Agata Christie'},
+        {value:'Simenon,Geroges', label: 'Geroges Simenon'},
+        {value:'Doyle,Arthur C.', label: 'Arthur C. Doyle'},
+    ]},
+    {value:'SF', label: 'Ciencia Ficcion', autores: [
+        {value:'Asimov,Isaac', label: 'Isaac Asimov'},
+        {value:'Gibson,William', label: 'William Gibson'},
+        {value:'Bradbury,Ray', label: 'Ray Bradbury'},
+    ]},
+    {value:'F', label: 'Fantasia', autores: [
+        {value:'Tolkien,JRR', label: 'JRR Tolkien'},
+        {value:'Stoker,Bran', label: 'Bran Stoker'},
+        {value:'Shelly,Mary', label: 'Mary Shelley'},
+    ]}
+]
