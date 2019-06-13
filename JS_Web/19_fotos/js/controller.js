22bbb4157b7faa8fdf7e307df614ebfc29b28e74
alce65 <alce65@hotmail.es>
2019-06-13 11:33:18 +0200

    Fotos: arquitectura mejorada

--- JS_Web/19_fotos/js/controller.js
+++ JS_Web/19_fotos/js/controller.js
@@ -1,48 +1,52 @@
 export function controller() {
 
+    // Variables de Estado
+
     const URL = 'https://jsonplaceholder.typicode.com/photos'
     const URL_ALBUM1 = URL+'?albumId=1'
-    const aFotos = []
-    let sectionFigures = document.querySelector('#figures')
-
-    /* fetch(URL).then(x)
-    
-    function x(response) {
-        return response.json()
-    } */
-
-    fetch(URL_ALBUM1)
-    .then( response => response.json())
-    .then( getFotos )
+    let aFotos = []
+    let itemActual = 0
 
     // Elementos del DOM
     const btnAdd = document.querySelector('#put')
-    let aBtnModificar 
-    let aBtnBorrar 
+    let aBtnModificar // No existe hasta pedir y renderizar datos
+    let aBtnBorrar // No existe hasta pedir y renderizar datos
+
+    // Elementos del DOM de los dialog (modales) inicialmente invisibles
 
     const addFotoDlg = document.querySelector('#addFotoDlg')
     const btnAddFoto = document.querySelector('#btnAddFoto')
     const btnCancelAddFoto = document.querySelector('#btnCancelAddFoto')
-
     const modifyFotoDlg = document.querySelector('#modifyFotoDlg')
     const btnModifyFoto = document.querySelector('#btnModifyFoto')
     const btnCancelModifyFoto = document.querySelector('#btnCancelModifyFoto')
+
 
     // Manejadores de eventos
     btnAdd.addEventListener('click', onAdd)
     btnAddFoto.addEventListener('click', onClickDlgAdd)
     btnCancelAddFoto.addEventListener('click', onClickDlgAdd)
-
     btnModifyFoto.addEventListener('click', onClickDlgModify)
     btnCancelModifyFoto.addEventListener('click', onClickDlgModify)
 
+    // Código inicial
 
-    /*  "albumId": 1,
-    */   
-    function getFotos(response) {
-        console.log(response)
+    getFotos(URL_ALBUM1)    
+
+    function getFotos(url) {
+        fetch(url)
+        .then( response => response.json())
+        .then( response => {
+            aFotos = response
+            console.log(aFotos)
+            renderFotos()
+        })
+    }
+
+    function renderFotos() {
+        let sectionFigures = document.querySelector('#figures')
         let html = ''
-        response.forEach( item => {
+        aFotos.forEach( item => {
                 html += `
                     <figure>
                         <a href="${item.url}">
@@ -56,8 +60,12 @@ export function controller() {
             });
 
         sectionFigures.innerHTML = html
+
+        // Elementos del DOM recien renderizada
         aBtnModificar = document.querySelectorAll('.post')
         aBtnBorrar= document.querySelectorAll('.delete')
+       
+        // Manejadores de eventos correspondientes
         aBtnModificar.forEach( item => item.addEventListener('click', onModify))
         aBtnBorrar.forEach( item => item.addEventListener('click', onDelete))
     }
@@ -67,59 +75,91 @@ export function controller() {
         addFotoDlg.showModal()
     }
 
+    function onModify(ev) {
+        itemActual =  ev.target.dataset.idDb
+        console.log('Modificando', itemActual)
+        modifyFotoDlg.showModal()
+    }
+
     function onClickDlgAdd(ev) {
 
         let id = ev.target.id // btnAddFoto o btnCancelAddFoto
         if (id == 'btnAddFoto') {
-            // añadir
-            let datos = {}
-            datos.albumId = document.querySelector('#albumId').value
-            datos.title = document.querySelector('#title').value
-            datos.thumbnailUrl = document.querySelector('#thumbnailUrl').value
-            datos.url = document.querySelector('#url').value
 
-            let myHeaders = new Headers({
-                "Content-Type": "application/json"
-            });
-              
+            if (document.querySelector('#title').value) {
+                // añadir
+                let datos = {}
+                datos.albumId = document.querySelector('#albumId').value
+                datos.title = document.querySelector('#title').value
+                datos.thumbnailUrl = document.querySelector('#thumbnailUrl').value
+                datos.url = document.querySelector('#url').value
 
-            fetch(URL, {method: 'POST', body: JSON.stringify(datos), headers: myHeaders})
-            .then (response => response.json())
-            .then (data =>  console.log(data))
+                document.querySelector('#title').value = ''
 
+                let myHeaders = new Headers({
+                    "Content-Type": "application/json"
+                });
+                
+
+                fetch(URL, {method: 'POST', body: JSON.stringify(datos), headers: myHeaders})
+                .then (response => response.json())
+                .then (data =>  {
+                    console.log(data)
+                    aFotos.push(data)
+                    renderFotos()
+                })               
+            }
         }
         addFotoDlg.close()
 
     }
 
-    function onModify(ev) {
-        console.log('Modificando', ev.target.dataset.idDb)
-        modifyFotoDlg.showModal()
-    }
-
     function  onClickDlgModify(ev) {
         let id = ev.target.id // 
         if (id == 'btnModifyFoto') {
-            let datos = {}
-            datos.title = document.querySelector('#modi_title').value
 
-            let myHeaders = new Headers({
-                "Content-Type": "application/json"
-            });
-            let url = URL + `/${ev.target.dataset.idDb}`
-            fetch(url, {method: 'PATCH', body: JSON.stringify(datos), headers: myHeaders})
-            .then (response => response.json())
-            .then (data =>  console.log(data))
+            if(document.querySelector('#modi_title').value) {
+                let datos = {}
+                datos.title = document.querySelector('#modi_title').value
+                document.querySelector('#modi_title').value = ''
+
+                let myHeaders = new Headers({
+                    "Content-Type": "application/json"
+                });
+                let url = URL + `/${itemActual}`
+                fetch(url, {method: 'PATCH', body: JSON.stringify(datos), headers: myHeaders})
+                .then (response => response.json())
+                .then (data =>  {
+                    let index
+                    aFotos.some( (item, i) => { if(item.id == itemActual) {
+                                                        index = i
+                                                        return true
+                    }})
+                    console.log(index)
+                    console.log(data)
+                    aFotos.splice(index,1,data)
+                    renderFotos()
+                })                
+            }
         }
         modifyFotoDlg.close()
     }
 
     function onDelete(ev) {
-        console.log('Borrando', ev.target.dataset.idDb)
-        let url = URL + `/${ev.target.dataset.idDb}`
+        itemActual =  ev.target.dataset.idDb
+        console.log('Borrando', itemActual)
+        let url = URL + `/${itemActual}`
         console.log(url)
-        fetch('url', {method: 'DELETE'})
-        .then (response => response.json())
+
+        let myHeaders = new Headers({
+            "Content-Type": "application/json"
+        });
+
+
+        fetch('url', {method: 'DELETE', headers: myHeaders})
+        .then (response => {
+            console.log(response)
+            response.json()})
         .then (data =>  console.log(data))
         .catch (error => console.error(error))
     }
