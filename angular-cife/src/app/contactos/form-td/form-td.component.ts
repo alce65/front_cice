93bdd54b08fda496c74a64e0ba109737503e53a3
alce65 <alce65@hotmail.es>
2019-06-27 14:50:29 +0200

    Formularios Final

--- angular-cife/src/app/contactos/form-td/form-td.component.ts
+++ angular-cife/src/app/contactos/form-td/form-td.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Curso, Turno, Sede, Alumno } from 'src/app/models/formulario.model';
 import { CURSOS, TURNOS, SEDES } from 'src/app/models/formulario.datos';
+
 
 @Component({
   selector: 'app-form-td',
@@ -14,6 +16,8 @@ export class FormTdComponent implements OnInit {
   turnos: Array<Turno>;
   sedes: Array<Sede>;
   alumno: Alumno;
+  @ViewChild('formCurso', {static: true})  formCurso: NgForm;
+  @ViewChild('refDlgConfirmar', {static: true}) dlgConfirmar: ElementRef;
 
   constructor() { }
 
@@ -22,6 +26,34 @@ export class FormTdComponent implements OnInit {
     this.turnos = TURNOS;
     this.sedes = SEDES;
     this.alumno = new Alumno();
+    this.alumno.curso = null;
+    this.alumno.sede = null;
+    this.alumno.turno = null;
+
+    console.log(this.formCurso);
   }
 
+  onSubmit(ev) {
+    ev.preventDefault();
+    this.dlgConfirmar.nativeElement.open = true;
+  }
+
+  onClickProbar() {
+    this.alumno = new Alumno('Pepe', 'Perez', '50283456Z',
+      'pp@sample.com', 'Curso@2345', '2000-12-12', '', true, true,
+      null, null, null);
+  }
+
+  onClickDlg(ev: boolean) {
+    if (ev) {
+      console.log('Datos enviados');
+      
+      /* this.alumno = new Alumno();
+      this.alumno.curso = null;
+      this.alumno.sede = null;
+      this.alumno.turno = null; */
+      this.formCurso.reset();
+    }
+    this.dlgConfirmar.nativeElement.open = false;
+  }
 }
